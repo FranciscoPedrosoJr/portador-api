dc76fc8e4b849c8fae2f515daac9e20fdae41d3f
@francisco.junior10 <francisco.junior@jazztech.com.br>
2023-06-30 10:47:11 -0300

    feat - Credit Card limit

--- src/main/java/com/jazztech/cardholder/applicationservice/service/CreditCardService.java
+++ src/main/java/com/jazztech/cardholder/applicationservice/service/CreditCardService.java
@@ -2,6 +2,7 @@ package com.jazztech.cardholder.applicationservice.service;
 
 import com.jazztech.cardholder.applicationservice.domain.entity.CreditCardDomain;
 import com.jazztech.cardholder.infrastructure.exceptions.CardHolderNotFoundException;
+import com.jazztech.cardholder.infrastructure.exceptions.CreditCardNotFoundException;
 import com.jazztech.cardholder.infrastructure.repository.entity.CardHolderEntity;
 import com.jazztech.cardholder.infrastructure.repository.entity.CreditCardEntity;
 import com.jazztech.cardholder.infrastructure.repository.util.CardHolderRepository;
@@ -38,17 +39,32 @@ public class CreditCardService {
     }
 
     private String generateCardNumber() {
-        Random random = new Random();
-        StringBuilder cardNumberBuilder = new StringBuilder();
-        for (int i = 0; i < 16; i++) {
-            int digit = random.nextInt(10);
+        final Random random = new Random();
+        final Integer CardMax = 16;
+        final StringBuilder cardNumberBuilder = new StringBuilder();
+        for (int i = 0; i < CardMax; i++) {
+            final int digit = random.nextInt(10);
             cardNumberBuilder.append(digit);
         }
         return cardNumberBuilder.toString();
     }
 
     private int generateCvv() {
-        Random random = new Random();
-        return 100 + random.nextInt(900);
+        final Random random = new Random();
+        final Integer CvvRandom = 100;
+        final Integer CvvBound = 900;
+        return CvvRandom + random.nextInt(CvvBound);
+    }
+
+    public void updateCardHolderLimit(UUID cardHolderId, UUID cardId, Double newLimit) {
+        final CardHolderEntity cardHolder = cardHolderRepository.findById(cardHolderId)
+                .orElseThrow(() -> new CardHolderNotFoundException("Card holder not found"));
+
+        final CreditCardEntity creditCard = creditCardRepository.findByCardIdAndCardHolderId(cardId, cardHolderId)
+                .orElseThrow(() -> new CreditCardNotFoundException("Credit card not found"));
+
+
+        cardHolder.setCardHolderLimit(newLimit);
+        creditCardRepository.save(creditCard);
     }
 }
